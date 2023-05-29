ecb7807a7a5fb016c5bc876a885e81bd9e792261
biser <bisserkr@gmail.com>
2023-05-29 21:53:43 +0300

    Begin implementation on bar collection

---
+++ trading/collect/bar/data.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+import io
+import struct
+
+
+class Bar:
+    MARKER = "BAR8"
+    BINARY_SIZE = 8 + 4*8 + 8
+    BINARY_FORMAT = "<QQQQQQ"
+    FIXED_POINT_MULTIPLIER = 1000000
+    
+    __slots__ = ("date", "iopen", "ihigh", "ilow", "iclose", "volume")
+    date: datetime
+    iopen: int
+    ihigh: int
+    ilow: int
+    iclose: int
+    volume: int
+
+    def __init__(self, date: datetime, iopen: int, ihigh: int, ilow: int, iclose: int, volume: int):
+        self.date = date
+        self.iopen = iopen
+        self.ihigh = ihigh
+        self.ilow = ilow
+        self.iclose = iclose
+        self.volume = volume
+
+    def write(self, stream: io.BytesIO):
+        buffer = struct.pack(
+            self.BINARY_FORMAT,
+            self.date.timestamp(),
+            self.iopen,
+            self.ihigh,
+            self.ilow,
+            self.iclose,
+            self.volume,
+        )
+        stream.write(buffer)
+
+    @classmethod
+    def read(cls, stream: io.BytesIO):
+        buffer = stream.read(cls.BINARY_SIZE)
+        data = struct.unpack(cls.BINARY_FORMAT, buffer)
+        return Bar(
+            date=datetime.fromtimestamp(data[0]),
+            iopen=data[1],
+            ihigh=data[2],
+            ilow = data[3],
+            iclose = data[4],
+            volume = data[5],
+        )
+    
+    def __repr__(self):
+        return f"Bar({self.date}, {self.iopen}, {self.ihigh}, {self.ilow}, {self.iclose}, {self.volume})"
+    
+    @property
+    def open(self):
+        return self.iopen / self.FIXED_POINT_MULTIPLIER
+    
+    @property.setter
+    def open(self, value):
+        self.iopen = int(value * self.FIXED_POINT_MULTIPLIER)
+
+    @property
+    def high(self):
+        return self.ihigh / self.FIXED_POINT_MULTIPLIER
+    
+    @property.setter
+    def high(self, value):
+        self.ihigh = int(value * self.FIXED_POINT_MULTIPLIER)
+
+    @property
+    def low(self):
+        return self.ilow / self.FIXED_POINT_MULTIPLIER
+    
+    @property.setter
+    def low(self, value):
+        self.ilow = int(value * self.FIXED_POINT_MULTIPLIER)
+
+    @property
+    def close(self):
+        return self.iclose / self.FIXED_POINT_MULTIPLIER
+    
+    @property.setter
+    def close(self, value):
+        self.iclose = int(value * self.FIXED_POINT_MULTIPLIER)
---
+++ trading/collect/bar/engine.py
@@ -0,0 +1,105 @@
+from datetime import datetime
+from enum import Enum
+import os
+import typing
+
+from collect.bar.data import Bar, Bar4, Bar8
+
+
+class BarFrequency(Enum):
+    MIN_1 = "MIN_1"
+    DAY_1 = "DAY_1"
+
+
+class BarReader:
+    namespace: str
+    symbol: str
+
+    def __init__(self, namespace: str, symbol: str):
+        self.namespace = namespace
+        self.symbol = symbol
+
+    def _get_years(self, freq: BarFrequency) -> typing.List[int]:
+        dir = self._bars_dir(freq)
+        if not os.path.isdir(dir):
+            return []
+        return [int(name[:-4]) for name in os.listdir(dir)]
+
+    def _bars_dir(self, freq: BarFrequency, year: int) -> str:
+        return os.path.join("data", "bars", self.namespace, self.symbol, freq.value)
+
+    def _bars_path(self, freq: BarFrequency, year: int) -> str:
+        return os.path.join("data", "bars", self.namespace, self.symbol, freq.value, f"{year}.bar")
+
+    def date_range(self, freq: BarFrequency) -> typing.Tuple[datetime, datetime]:
+        years = self._get_years(freq)
+        first_year = min(years)
+        last_year = max(years)
+        path = self._bars_path(freq, first_year)
+        with open(path, "rb") as fh:
+            fh.seek(4, os.SEEK_SET)
+            first_bar = Bar.read(fh)
+        path = self._bars_path(freq, last_year)
+        with open(path, "rb") as fh:
+            fh.seek(-Bar.BINARY_SIZE, os.SEEK_END)
+            last_bar = Bar.read(fh)
+        return (first_bar.date, last_bar.date)
+
+    def count(self, freq: BarFrequency):
+        count = 0
+        years = self._get_years(freq)
+        for year in years:
+            path = self._bars_path(freq, year)
+            count += (os.path.getsize(path) - 4) // Bar.BINARY_SIZE
+        return count
+
+    def read_all(self, freq: BarFrequency):
+        result = []
+        years = self._get_years(freq)
+        for year in years:
+            path = self._bars_path(freq, year)
+            count = (os.path.getsize(path) - 4) // Bar.BINARY_SIZE
+            with open(path, "rb") as fh:
+                fh.seek(4, os.SEEK_SET)
+                result.extend(Bar.read(fh) for _ in range(count))
+        return result
+
+    def read_since(self, freq: BarFrequency, start: datetime):
+        years = self._get_years(freq)
+        years = [year for year in years if year >= start.year]
+        result = []
+        for year in years:
+            path = self._bars_path(freq, year)
+            count = (os.path.getsize(path) - 4) // Bar.BINARY_SIZE
+            with open(path, "rb") as fh:
+                fh.seek(4, os.SEEK_SET)
+                bars = [Bar.read(fh) for _ in range(count)]
+                bars = [bar for bar in bars if bar.date >= start]
+                result.extend(bars)
+        return result
+
+
+class BarWriter:
+    namespace: str
+    symbol: str
+
+    def __init__(self, namespace: str, symbol: str):
+        self.namespace = namespace
+        self.symbol = symbol
+
+    def store(self, freq: BarFrequency, bars: typing.List[Bar]):
+        if not bars:
+            return
+        path = os.path.join("data", "bars", self.namespace, self.symbol, f"{freq.value}.bar")
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        exists = os.path.isfile(path)
+        with open(path, "ab") as fh:
+            if not exists:
+                fh.write(Bar.MARKER.encode("ascii"))
+            else:
+                existing_marker = fh.read(4).decode("ascii")
+                if existing_marker != Bar.MARKER:
+                    raise Exception(f"Existing marker {existing_marker} does not match {Bar.MARKER}")
+                fh.seek(0, os.SEEK_END)
+            for bar in bars:
+                bar.write(fh)
